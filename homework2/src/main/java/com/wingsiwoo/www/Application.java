ed94af731d8444a18a3d3419755340eea78e0393
WingsiWoo <wingsiwoo@outlook.com>
2021-10-13 20:39:49 +0800

    feat：完成界面代码

--- homework2/src/main/java/com/wingsiwoo/www/Application.java
+++ homework2/src/main/java/com/wingsiwoo/www/Application.java
@@ -1,10 +1,14 @@
+package com.wingsiwoo.www;
+
 import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * @author WingsiWoo
  * @date 2021/10/13
  */
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
--- homework2/src/main/java/com/wingsiwoo/www/view/MainFrame.java
+++ homework2/src/main/java/com/wingsiwoo/www/view/MainFrame.java
@@ -1,15 +1,12 @@
 package com.wingsiwoo.www.view;
 
-import java.awt.*;
-import java.util.*;
+import com.wingsiwoo.www.service.EncryptService;
+import org.springframework.stereotype.Component;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.swing.*;
-
-import com.wingsiwoo.www.service.EncryptService;
-import net.miginfocom.swing.*;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+import java.awt.*;
 
 /**
  * @author WingsiWoo
@@ -39,6 +36,55 @@ public class MainFrame extends JFrame {
 
     @PostConstruct
     private void initComponents() {
+        JFrame mainFrame = new JFrame();
+        Container container = mainFrame.getContentPane();
+        // create component
+        initField();
 
+        mainFrame.setTitle("ENCRYPTION");
+        mainFrame.setSize(800, 400);
+        // Set the initial position of the main interface
+        mainFrame.setLocationRelativeTo(mainFrame.getOwner());
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+        mainFrame.setResizable(false);
+        container.setLayout(null);
+
+        // set the position of the components
+        Font textFiledFont = new Font("alias", Font.PLAIN, 17);
+        // textField
+        textField.setEditable(true);
+        textField.setBounds(170, 130, 280, 50);
+        textField.setFont(textFiledFont);
+        container.add(textField);
+
+        // caesarButton
+        caesarButton.setText("CAESAR");
+        caesarButton.setBounds(580, 60, 110, 45);
+        container.add(caesarButton);
+
+        // playfairButton
+        playfairButton.setText("PLAYFAIR");
+        playfairButton.setBounds(580, 140, 110, 45);
+        container.add(playfairButton);
+
+        // hillButton
+        hillButton.setText("HILL");
+        hillButton.setBounds(580, 220, 110, 45);
+        container.add(hillButton);
+    }
+
+    private void initField() {
+        textField = new JTextField();
+        caesarButton = new JButton();
+        playfairButton = new JButton();
+        hillButton = new JButton();
+    }
+
+    private void registerListener() {
+        String text = textField.getText().trim();
+        // this.caesarButton.addMouseListener();
+        // this.playfairButton.addMouseListener();
+        // this.hillButton.addMouseListener();
     }
 }
